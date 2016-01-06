59f8fc4b4c4bd697ebbfb13bb9bb28ef1c5498e0
zoharSwisa <zoara7@gmail.com>
2016-01-06 20:33:33 +0200

    Create ProfileActivity and 'TripReportActivity' in addition edit 'signUpAvtivity'

---
+++ app/src/main/java/com/example/nofarcohenzedek/dogo/MyProfileActivity.java
@@ -0,0 +1,14 @@
+package com.example.nofarcohenzedek.dogo;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class MyProfileActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_profile);
+    }
+
+}
