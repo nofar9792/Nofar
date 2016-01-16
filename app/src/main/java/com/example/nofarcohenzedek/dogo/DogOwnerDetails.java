9c75afe5c289e9b9f7b71247767584d7d46131f6
zoharSwisa <zoara7@gmail.com>
2016-01-16 13:48:09 +0200

    Create DogOwnerDetails screen

---
+++ app/src/main/java/com/example/nofarcohenzedek/dogo/DogOwnerDetails.java
@@ -0,0 +1,92 @@
+package com.example.nofarcohenzedek.dogo;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Activity;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+import com.example.nofarcohenzedek.dogo.Model.Dog;
+import com.example.nofarcohenzedek.dogo.Model.DogOwner;
+import com.example.nofarcohenzedek.dogo.Model.DogSize;
+import com.example.nofarcohenzedek.dogo.Model.DogWalker;
+import com.example.nofarcohenzedek.dogo.Model.Model;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public class DogOwnerDetails extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dog_owner_details);
+
+        Intent intent = getIntent();
+        final String dogOwnerId = intent.getStringExtra("id");
+
+        final TextView firstName = (TextView) findViewById(R.id.firstNameInDetails);
+        final TextView lastName = (TextView) findViewById(R.id.lastNameInDetails);
+        final TextView city = (TextView) findViewById(R.id.cityInDetails);
+        final TextView address = (TextView) findViewById(R.id.addressInDetails);
+        final TextView dogName = (TextView) findViewById(R.id.dogNameForDetais);
+        final TextView dogAge = (TextView) findViewById(R.id.dogAgeForDetais);
+        final RadioButton isBig = (RadioButton) findViewById(R.id.isBigForDetais);
+        final RadioButton isMedium = (RadioButton) findViewById(R.id.isMediumForDetais);
+        final RadioButton isSmall = (RadioButton) findViewById(R.id.isSmallForDetais);
+        final ImageView dogPic = (ImageView) findViewById(R.id.dogPicForDetais);
+
+        Model.getInstance().getDogOwnerById(Long.parseLong(dogOwnerId), new Model.GetDogOwnerListener() {
+            @Override
+            public void onResult(DogOwner dogOwner) {
+                firstName.setText(dogOwner.getFirstName());
+                lastName.setText(dogOwner.getLastName());
+                city.setText(dogOwner.getCity());
+                address.setText(dogOwner.getAddress());
+                Dog dog = dogOwner.getDog();
+                dogName.setText(dog.getName());
+                dogAge.setText(String.valueOf(dog.getAge()));
+
+                // Create ImageView
+                // // TODO: 16/01/2016 load the image of dog 
+//                InputStream imageStream = null;
+//                try {
+//                    Uri selectedImage = Uri.parse("content://com.google.android.apps.photos.contentprovider/0/1/content%3A%2F%2Fmedia%2Fexternal%2Fimages%2Fmedia%2F32317/NO_TRANSFORM/66117322");
+//                    imageStream = getContentResolver().openInputStream(selectedImage);
+//                } catch (FileNotFoundException e) {
+//                    e.printStackTrace();
+//                }
+//                dogPic.setImageBitmap(BitmapFactory.decodeStream(imageStream));
+//
+//                dogPic.setImageURI(Uri.fromFile(new File(dog.getPicRef())));
+
+                // Check which size the dog is.
+                if (dog.getSize().name().equals(DogSize.Large.name()))
+                {
+                    isBig.setChecked(true);
+                    isMedium.setChecked(false);
+                    isSmall.setChecked(false);
+                }
+                else if (dog.getSize().name().equals(DogSize.Medium.name()))
+                {
+                    isBig.setChecked(false);
+                    isMedium.setChecked(true);
+                    isSmall.setChecked(false);
+                }
+                else if (dog.getSize().name().equals(DogSize.Small.name()))
+                {
+                    isBig.setChecked(false);
+                    isMedium.setChecked(false);
+                    isSmall.setChecked(true);
+                }
+            }
+        });
+    }
+
+}
